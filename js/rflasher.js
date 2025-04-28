45507725ded02bbd915edff1f13b199360189c3e
x01rrr <x01r@gmx.com>
2025-04-29 01:29:38 +0300

    Update rflasher.js

--- js/rflasher.js
+++ js/rflasher.js
@@ -81,9 +81,11 @@ function KokoLatestLink(port,device) {
 
 var repo_url = 'https://api.github.com/repos/justcallmekoko/ESP32Marauder/releases';
 	
+var devices = 'ESP32';
 var offset = '4096';
 if (port == 'boardS3') {
 	offset = '0';
+	devices = 'ESP32-S3';
 }
 	
 fetch(repo_url)
@@ -109,7 +111,7 @@ fetch(repo_url)
 			version: "",
 			home_assistant_domain: "Rabbit Labs",
 			funding_url: "https://rabbit-labs.com",
-			builds: [ { "chipFamily": "ESP32", "parts": [ { "path": "../bins/rocket/bootloader.bin", "offset": offset }, { "path": "../bins/rocket/partitions.bin", "offset": 32768 }, { "path": latest_bin, "offset": 65536 } ] } ]
+			builds: [ { "chipFamily": devices, "parts": [ { "path": "../bins/rocket/bootloader.bin", "offset": offset }, { "path": "../bins/rocket/partitions.bin", "offset": 32768 }, { "path": latest_bin, "offset": 65536 } ] } ]
 
 			}, 'manifestss');
 		}
